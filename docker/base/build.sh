afdcb5a7d6d4e6ae202a6c2cb545e55286190504
André Lorenzen <andrelorenzen@gmx.de>
2022-06-03 13:02:34 +0200

    added docker build param to pass a specific ID for the "sgx" user

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -2,9 +2,11 @@
 
 IMAGE="notifierman/openenclave-sdk"
 TAG="base-0.1"
+SGX_USER_ID=1000
 
 base_folder=$(dirname $(readlink -f $0))
 
 docker build \
+    --build-arg SGX_USER_ID=${SGX_USER_ID} \
     -t ${IMAGE}:${TAG} \
     ${base_folder}/
